2ecd71d521a42a3a39e6fd85cf41f6e210d5b76f
subhandp <subhan.dinda.putra@gmail.com>
2020-08-27 11:08:52 +0800

    remove html tag

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,6 @@
 <template>
   <div id="app">
 
-<html lang="en">
-	<head>
-
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Blog</title>
-	</head>
-	<body>
-
 		<LayoutHeader></LayoutHeader>
 		
 		<LayoutNavbar></LayoutNavbar>
@@ -38,8 +28,7 @@
 
   <LayoutFooter></LayoutFooter>
 	
-	</body>
-</html>
+
 
   </div>
 </template>
@@ -50,10 +39,6 @@ import LayoutFooter from './views/layouts/LayoutFooter'
 import LayoutHeader from './views/layouts/LayoutHeader'
 import LayoutNavbar from './views/layouts/LayoutNavbar'
 import LayoutSidebar from './views/layouts/LayoutSidebar'
-// import Page from './views/Page'
-// import Albums from './assets/data/albums'
-// import Photos from './assets/data/photos'
-// import Posts from './assets/data/posts'
 
 
 
